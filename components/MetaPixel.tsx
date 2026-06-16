fe6a95f750d13cb81cd957cb6823cededd58fc2f
Jonatan <qjonatan188@gmail.com>
2026-06-16 07:06:56 -0300

    Add MetaPixel component for Facebook tracking

---
+++ components/MetaPixel.tsx
@@ -0,0 +1,45 @@
+// components/MetaPixel.tsx
+'use client'
+
+import { useEffect } from 'react'
+import { usePathname } from 'next/navigation'
+
+const PIXEL_ID = '1316975003913224'
+
+declare global {
+  interface Window {
+    fbq: any
+  }
+}
+
+export default function MetaPixel() {
+  const pathname = usePathname()
+
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.innerHTML = `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '${PIXEL_ID}');
+    `
+    document.head.appendChild(script)
+
+    return () => {
+      document.head.removeChild(script)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (window.fbq) {
+      window.fbq('track', 'PageView')
+    }
+  }, [pathname])
+
+  return null
+}
